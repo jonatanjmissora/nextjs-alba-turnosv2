6faa42bd8183d180047e29ef40e37dfc225e6f6c
Jonatan <jonatanjmissora@gmail.com>
2026-02-01 16:13:02 -0300

    pnpm change, corregimos error en el Turnoshema, el id

--- src/lib/schema-turno.ts
+++ src/lib/schema-turno.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 
+const actualYear = new Date().getFullYear();
+
 export const schemaTurno = z.object({
-    id: z.number().min(202500000000).max(202512311900),
+    id: z.number().min(actualYear * 100000000).max(actualYear * 100000000 + 12311900),
     servicio: z.number().min(0).max(100),
     fecha: z.string().trim().min(10).max(10),
     hora: z.string().trim().min(5).max(5),
